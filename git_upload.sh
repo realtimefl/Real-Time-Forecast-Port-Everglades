790c9c1055464e561d254d61a08a913e942f2b0a
Ahsan <mhabib2014@my.fit.edu>
2019-08-05 01:44:18 -0400

    update commit

--- git_upload.sh
+++ git_upload.sh
@@ -1,6 +1,8 @@
 
 git init
-git remote add origin https://github.com/realtimefl/PortEverglades.git
+#git remote add origin https://github.com/realtimefl/PortEverglades.git
 git add .
 git commit -m "update commit"
 git push -u origin master
+
+# use 'git pull origin master' command if 'error: failed to push some refs to ... ' appears
