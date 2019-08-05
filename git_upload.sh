6ffb83cb44b50952a4a51e5a350b1be4598c5df4
Ahsan <mhabib2014@my.fit.edu>
2019-08-05 12:19:46 -0400

    update commit

--- git_upload.sh
+++ git_upload.sh
@@ -2,6 +2,9 @@
 git init
 git pull origin master 
 #git remote add origin https://github.com/realtimefl/PortEverglades.git
+rm waterlevel_evergladesinlet.jpg waterlevel_hillsboroinlet.jpg waterlevel_plantriver.jpg waterlevel_porteverglades.jpg
+
+
 git add .
 git commit -m "update commit"
 git push -u origin master
