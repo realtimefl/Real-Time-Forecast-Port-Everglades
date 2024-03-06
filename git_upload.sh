e1c5633fd7d8a1edf46a3ae9b17152840f22e0ad
mhabib2014@my.fit.edu <mhabib2014@my.fit.edu>
2024-03-06 12:27:36 -0500

    update commit

--- git_upload.sh
+++ git_upload.sh
@@ -1,6 +1,6 @@
 
 git init
-git pull origin master 
+#git pull origin master 
 #git remote add origin https://github.com/realtimefl/PortEverglades.git
 rm waterlevel_evergladesinlet.jpg waterlevel_hillsboroinlet.jpg waterlevel_plantriver.jpg waterlevel_porteverglades.jpg
 
